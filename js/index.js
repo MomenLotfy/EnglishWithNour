a6c65ce1302aad5b4b1fd6a41284039861a7e721
mo'men lotfy <momen.elpesa123@gmail.com>
2025-09-26 13:11:12 +0300

    Delete js/index.js

--- js/index.js
+++
@@ -1,9 +0,0 @@
-import A1 from "../Grammer/A1.js";
-import A2 from "./Grammer/A2.js";
-import B1 from "./Grammer/B1.js";
-import B2 from "./Grammer/B2.js";
-
-
-const grammarData = { A1, A2, B1, B2 };
-
-export default grammarData;
